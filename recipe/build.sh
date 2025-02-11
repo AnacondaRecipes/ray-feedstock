0780226a1187ade5706d6b54cdfbe21782ce9aa1
anaobi <anaobii@gmail.com>
2025-02-11 15:16:09 +0000

    first build

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-$PYTHON -m pip install --no-deps --no-build-isolation --ignore-installed "${SRC_DIR}/ray-2.42.0-cp311-cp311-manylinux2014_x86_64.whl"
+$PYTHON -m pip install --no-deps --no-build-isolation --ignore-installed "${SRC_DIR}/ray-2.42.0-cp312-cp312-manylinux2014_x86_64.whl"
